7bb40a573369a7fa28bfb5ad99500ddc2c395e4a
Martina Juri <martina.juri@mi.unc.edu.ar>
2023-04-30 17:33:50 -0300

    Update List.cpp

---
+++ src/structure/List.cpp
@@ -0,0 +1,138 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include "Nodo.cpp"
+
+using namespace std;
+
+template <class T> class Lista {
+private: Nodo<T>* czo;
+     
+public:
+    Lista() { czo = new Nodo<T>(); };
+    Lista(Nodo<T>* n) { czo = n; };
+    ~Lista();
+    void add(T d); 
+    bool isEmpty(void){return czo->es_vacio();};
+    T cabeza(void); //retorn primer nodo
+    Lista* resto(void); //retorna el puntero al "resto" de la lista
+                        //resto= lo que queda de la lista sin la cabeza
+    string toPrint(string p);
+    T suma(T i);
+    int size();
+    void borrar(void); //borra la cabeza
+    void borrar_last();//borra el ultimo
+    void concat(Lista<T>* l1);// le transfiere los datos de l1 a this
+    Lista<T>* copy(void);// hace una copia de la lista
+    void tomar(int n);//deja "vivos" los n primeros nodos y borra el resto
+   
+};
+template <class T>
+void Lista<T>::add(T d) //100
+{
+    Nodo<T>* nuevo = new Nodo<T>(d);
+    nuevo->set_next(czo);
+    czo = nuevo;
+}
+
+template <class T>
+T Lista<T>::cabeza(void)
+{
+    if (this->isEmpty()) {
+        cout << " Error, Cabeza de lista vacia";
+        return NULL;
+    }
+    return czo->get_dato();
+}
+
+template <class T>
+Lista<T>* Lista<T>::resto(void)
+{
+    Lista* l = new Lista(czo->get_next());
+    return (l);
+}
+
+template <class T>
+string Lista<T>::toPrint(string p)
+{
+    if (this->isEmpty()) {
+        return p;
+    }
+    else {
+        //std::ostringstream stm;
+        ostringstream stm;
+        stm << this->cabeza() << "-" << this->resto()->toPrint(p) << endl;
+        //cout<<endl<<" stm.str()= "<<stm.str()<<endl;
+        return stm.str();
+    }
+}
+
+template <class T>
+T Lista<T>::suma(T i)
+{    //cout<<" i al entrar= "<<i<<endl;
+    if (this->isEmpty()) {
+        return i;
+    }
+    else {
+
+        //cout<<"this->cabeza()= "<<this->cabeza()<<endl;   
+        return this->resto()->suma(i + this->cabeza());
+    }
+}
+
+/*
+void addOrdenado(T d)// suma nodos ordenados de menor a mayor
+bool esta(T d); //retorna true cuando d está en la lista
+void borrarDato(T d)// borra el nodo que contiene a d
+*/
+
+template <class T> int Lista<T>::size()
+{
+    if (this->isEmpty()) return 0;
+    return 1 + this->resto()->size();
+}
+
+
+
+template <class T> void Lista<T>::borrar(void)
+{ //borra el nodo cabeza
+    if (!this->isEmpty()) {
+        Nodo<T>* tmp = czo;
+        czo = czo->get_next();
+        delete tmp;
+    }
+}
+
+template <class T> void Lista<T>::borrar_last()
+{ // borra el ultimo nodo
+    if (!this->isEmpty()) {
+        if ((czo->get_next())->get_next() == NULL) {
+            delete czo->get_next();
+            czo->set_next(NULL);
+        }
+        else this->resto()->borrar_last();
+    }
+}
+
+template <class T> void Lista<T>::concat(Lista<T>* l1)
+{// le transfiere los datos de l1 a this
+    if (!(l1->isEmpty())) {
+        this->concat(l1->resto());
+        this->add(l1->cabeza());
+    }
+}
+
+template <class T> Lista<T>* Lista<T>::copy(void)
+{
+    Lista<T>* aux = new Lista();
+    aux->concat(this);
+    return aux;
+}
+
+template <class T> void Lista<T>::tomar(int n)
+{ //deja "vivos" los n primeros nodos y borra el resto
+    if (this->size() > n) {
+        this->borrar_last();
+        this->tomar(n);
+    }
+}
