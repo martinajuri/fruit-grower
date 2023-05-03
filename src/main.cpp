4b2d229c895a9e552c00dfc9c981ef1a05f63618
Martina Juri <martina.juri@mi.unc.edu.ar>
2023-05-02 22:34:35 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include "model\Client.hpp"
-/*#include "model\Crate.h"
+/*
+#include "model\Crate.hpp"
+#include "structure\CrateStack.cpp"
 #include "utils\enum.hpp"
-#include "orders\RetailOrder.h"
-#include "orders\WholesaleOrder.h"
-#include "stock\OrderManager.h"
-#include "stock\Warehouse.h"
+#include "orders\RetailOrder.hpp"
+#include "orders\WholesaleOrder.hpp"
+#include "stock\OrderManager.cpp"
+#include "stock\Warehouse.cpp"
+#include "structure\List.cpp"
+#include "structure\Queue.hpp"
 */
 
-#include "structure\List.hpp"
-#include "structure\Queue.hpp"
+
 
 using namespace std;
 int main( int argc, char *argv[])
@@ -18,33 +21,24 @@ int main( int argc, char *argv[])
     Warehouse *deposito = new Warehouse();
     OrderManager *cajero = new OrderManager(*deposito);
 
-    Crate *crate1 = new Crate("crate1", FruitType::APPLE, 15.0f);
-    Crate *crate2 = new Crate("crate2", FruitType::BANANA,2.0f);
+    Queue<Client> *cola = new Queue<Client>; 
 
     RetailOrder *orden1 = new RetailOrder(2.0,*client1,FruitType::APPLE);
     WholesaleOrder *orden2 = new WholesaleOrder(5,*client2,FruitType::BANANA);
-    */
-
-   /*
-   T tope() { return this->last(); };
-    bool colavacia(void) { return this->isEmpty(); };
-    T ultimo(void) { return this->cabeza(); };
-    string toPrint(string s) { return this->toPrint(s); };
-    */
 
     //List<int> *lista = new List<int>;
-    Queue<Client> *cola = new Queue<Client>;
+    Crate *crate1 = new Crate("crate1", FruitType::APPLE, 15.0f);
+    Crate *crate2 = new Crate("crate2", FruitType::BANANA,2.0f);
+
+    */
+
     Client *fer = new Client("Fer");
     Client *marcos = new Client("Marcos");    
     Client *marti = new Client("Marti");
-    Client *sofia = new Client("Sofia");
+    Client *sofia = new Client("Sofia");    
 
+    //CrateStack *pila = new CrateStack(FruitType::APPLE);
 
-    cola->encolar(*fer);
-    cola->encolar(*marcos);
-    cola->encolar(*marti);
-    cola->encolar(*sofia);
+  
 
-    cola->imprimirQ();
-    cola->ultimo().imprimir();
 }
